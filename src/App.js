6d75f5d7bac7cb4f682b0d5c0380e79a89d9c32b
Thienkim Nguyen <uprise6104@gmail.com>
2024-05-25 21:22:34 -0400

    Added Pagination Feature

--- src/App.js
+++ src/App.js
@@ -38,6 +38,27 @@ function App() {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data });
 
+  // Add state management for pagination
+  const [currentPage, setCurrentPage] = React.useState(0);
+  //const entriesPerPage = 10;
+  const [entriesPerPage, setEntriesPerPage] = React.useState(10);
+
+  // Paginate Data
+  const paginateData = React.useMemo(() => {
+    const startIndex = currentPage * entriesPerPage;
+    const lastIndex = startIndex + entriesPerPage;
+    return rows.slice(startIndex, lastIndex);
+  }, [currentPage, entriesPerPage, rows]);
+
+  // Pagination Handlers
+  const nextPage = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, Math.ceil(rows.length / entriesPerPage) - 1));
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 0));
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -54,7 +75,7 @@ function App() {
             ))}
           </thead>
           <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
+            {paginateData.map((row) => {
               prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
@@ -66,6 +87,27 @@ function App() {
             })}
           </tbody>
         </table>
+        {/*Pagination Section*/}
+        <div className="pagination">
+          <div>
+            Rows per page:
+            <select value={entriesPerPage} onChange={(e) => setEntriesPerPage(Number(e.target.value))}>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+          </div>
+          <span>
+            {currentPage * entriesPerPage + 1} - {Math.min((currentPage + 1) * entriesPerPage, rows.length)} of {rows.length}
+          </span>
+          <button onClick={prevPage} disable={currentPage === 0}>
+            Previous
+          </button>
+          <button onClick={nextPage} disabled={currentPage >= Math.ceil(rows.length / entriesPerPage) - 1}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
